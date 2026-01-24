c2f21114be1066fc652095a39a0c4e9e53b68e70
Samuel Robinson <sirschool07@gmail.com>
2026-01-24 14:53:30 -0500

    Initial commit

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,13 @@
+require("config.lazy")
+
+-- Disable word wrapping
+vim.opt.wrap = false
+
+-- Enable line numbers
+vim.opt.number = true
+
+-- Tab settings
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
