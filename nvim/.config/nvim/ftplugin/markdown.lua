4a30246f3b40ac6c809ea9ee1155d49596e29aae
Samuel Robinson <sirschool07@gmail.com>
2026-03-17 16:21:28 -0400

    big ass changes ig???

--- nvim/.config/nvim/ftplugin/markdown.lua
+++ nvim/.config/nvim/ftplugin/markdown.lua
@@ -1,5 +1,10 @@
 vim.treesitter.start()
 
--- Enable word wrapping
+-- Enable word wrapping and smooth scroll
 vim.opt_local.wrap = true
 vim.opt_local.linebreak = true
+vim.opt_local.smoothscroll = true
+
+-- Break indent settings
+vim.opt_local.breakindent = true
+vim.opt_local.breakindentopt = 'list:-1'
