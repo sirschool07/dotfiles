54ce2bbd57a77175e4dbf4f3c8be6b699b934b3f
sirschool07 <sirschool07@gmail.com>
2026-04-28 22:41:42 -0400

    hid all class files from neotree

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -1,12 +1,21 @@
 return {
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      "nvim-tree/nvim-web-devicons", -- optional, but recommended
-    },
-    lazy = false, -- neo-tree will lazily load itself
-  }
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    "nvim-tree/nvim-web-devicons", -- optional, but recommended
+  },
+  lazy = false, -- neo-tree will lazily load itselfconfig = function()
+  config = function()
+    require('neo-tree').setup {
+      filesystem = {
+        filtered_items = {
+          hide_by_pattern = {
+            "*.class"
+          }
+        }
+      }
+    }
+  end,
 }
